ff03e486c2c07ff224693333da670c2bc3da9c56
Itamichan <cristinagarbuz@gmail.com>
2020-04-05 12:06:28 +0200

    created the verify_user function

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 from collections import namedtuple
+import base64
 
 import psycopg2
 
@@ -27,9 +28,9 @@ class Database:
 
             # hashing the user's password, adding salt and iterations
             salt = os.urandom(32)
-            hash_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+            hash_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000).hex()
 
-            cursor.execute(sql, (username, firstname, lastname, email, hash_password, salt,))
+            cursor.execute(sql, (username, firstname, lastname, email, hash_password, salt.hex()))
             self.connection.commit()
 
     def get_user_by_name(self, username):
@@ -40,9 +41,20 @@ class Database:
             if result is not None:
                 return User(id=result[0], username=username, email=result[2], password=result[3], salt=result[4])
 
+    def verify_user(self, username, password):
+        """ This function takes username and password as parametres and checks if a user with such a password exists in the database"""
+        user = self.get_user_by_name(username)
+        if user:
+            salt = bytearray.fromhex(user.salt)
+            hash_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000).hex()
+            if user.password == hash_password:
+                return user
+
+
 # testing if the class functions are correctly executed
 if __name__ == '__main__':
     db_instance = Database()
 
-    db_instance.create_user('testtest3', 'test', 'test', 'test@gmail.com', 'testtest')
+    db_instance.create_user('testtest4', 'test', 'test', 'test@gmail.com', 'testtest')
+    print(db_instance.verify_user('testtest4', 'testtest'))
     db_instance.close_connection()
