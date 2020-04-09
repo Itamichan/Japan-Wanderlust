eae5e1b314efee8bfd08b77161f34f189ce913db
Itamichan <cristinagarbuz@gmail.com>
2020-04-09 16:33:53 +0200

    created attraction_types.py view

---
+++ src/views/attraction_types.py
@@ -0,0 +1,50 @@
+from flask import jsonify, request
+from flask.views import MethodView
+
+from database.attractions_database import AttractionsDatabase
+from errors import response_500, response_404
+
+
+class TypesView(MethodView):
+
+    def get(self):
+        """
+
+        @api {GET} /api/v1/types/ Get Attraction Types
+        @apiVersion 1.0.0
+
+        @apiName AttractionTypes
+        @apiGroup Attractions
+
+        @apiSuccess {Object[]} attraction_types             List with all attraction types
+        @apiSuccess {Integer} attraction_types.type_id      Attraction type id
+        @apiSuccess {String} attraction_types.type_name     Attraction type name
+
+        @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            [
+                {
+                    "type_id": 1,
+                    "type_name": "temples"
+                },
+                {
+                    "type_id": 2,
+                    "type_name": "shrines"
+                }
+            ]
+        }
+
+        @apiError (InternalServerError 500) {Object} InternalServerError
+
+        """
+        try:
+            db_instance = AttractionsDatabase()
+            attraction_types_list = db_instance.get_attraction_types()
+            db_instance.close_connection()
+
+            return jsonify({
+                attraction_types_list
+            })
+        except:
+            return response_500()
