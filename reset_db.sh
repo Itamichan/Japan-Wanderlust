13546ecb0b0c7f4b16426015e6b0998da324859d
Itamichan <cristinagarbuz@gmail.com>
2020-04-12 14:50:21 +0200

    debugging trevis setup 3

---
+++ reset_db.sh
@@ -0,0 +1,3 @@
+sudo runuser -l  postgres -c 'psql -c "drop database unittest_backend;"'
+sudo runuser -l  postgres -c 'psql -c "create database unittest_backend;"'
+sudo runuser -l  postgres -c 'psql -c "grant all privileges on database unittest_backend to test_user;"'
