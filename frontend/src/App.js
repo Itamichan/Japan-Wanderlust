5f3895ee2aa3e9b2532e2ab38d87a6e6223dd86a
Itamichan <cristinagarbuz@gmail.com>
2020-04-14 11:47:10 +0200

    initial set-up fo react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    I am your frontend!
-                </p
-                </header>
+            <p>
+                I am your frontend!
+            </p>
         </div>
 );
 }
