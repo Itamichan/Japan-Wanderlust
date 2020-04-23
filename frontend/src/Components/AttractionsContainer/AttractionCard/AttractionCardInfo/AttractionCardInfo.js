11f0e91defab4e29a9bcdee5241ec206aa942794
Itamichan <cristinagarbuz@gmail.com>
2020-04-23 16:50:05 +0200

    created UserAttractionsList comp

--- frontend/src/Components/AttractionsContainer/AttractionCard/AttractionCardInfo/AttractionCardInfo.js
+++ frontend/src/Components/AttractionsContainer/AttractionCard/AttractionCardInfo/AttractionCardInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const AttractionCardInfo = (props) => {
+    //todo to implement
     return (
         <div/>
     )
---
+++ frontend/src/Components/Navigation/Trips/TripAttractionsInfo/UserAttractionsList/UserAttractionsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserAttractionsList = (props) => {
+    return (
+        <div/>
+    )
+};
+
+export default UserAttractionsList;
