1e89f75c6469dcab9197115eb971780684199987
Itamichan <cristinagarbuz@gmail.com>
2020-07-14 13:55:50 +0200

    use open graph tags in index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,21 @@ import {createStore} from "redux";
 import rootReducer from "./reducers";
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
-import {faHeart as fasFaHeart, faChevronLeft, faEnvelope, faEdit, faWindowClose, faYenSign, faStreetView, faUsers, faTimes, faInfo, faPlus} from '@fortawesome/free-solid-svg-icons'
+import {
+    faHeart as fasFaHeart,
+    faChevronLeft,
+    faEnvelope,
+    faEdit,
+    faWindowClose,
+    faYenSign,
+    faStreetView,
+    faUsers,
+    faTimes,
+    faInfo,
+    faPlus
+} from '@fortawesome/free-solid-svg-icons'
 import {faCalendarAlt, faTimesCircle, faTrashAlt} from '@fortawesome/free-regular-svg-icons'
+import Helmet from "react-helmet";
 
 library.add(fab, fasFaHeart, faChevronLeft, faEnvelope, faWindowClose, faEdit, faTrashAlt, faYenSign, faStreetView, faUsers, faInfo, faPlus, faTimes, faCalendarAlt, faTimesCircle);
 
